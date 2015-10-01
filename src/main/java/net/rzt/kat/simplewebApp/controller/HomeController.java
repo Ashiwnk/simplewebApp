12b02d75a1de78e5db123e3510cbcffe853eaf4b
Ashwinkrishna <mailashwink@gmail.com>
2015-10-01 15:21:18 +0530

    deploy

---
+++ src/main/java/net/rzt/kat/simplewebApp/controller/HomeController.java
@@ -0,0 +1,23 @@
+package net.rzt.kat.simplewebApp.controller;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping(value="/")
+	public ModelAndView test(HttpServletResponse response) throws IOException{
+		return new ModelAndView("home");
+	}
+	@RequestMapping(value="/notification")
+	public ModelAndView notification(HttpServletResponse request , HttpServletResponse response) throws IOException{
+		System.out.println("request came");
+		return new ModelAndView("home");
+	}
+}
